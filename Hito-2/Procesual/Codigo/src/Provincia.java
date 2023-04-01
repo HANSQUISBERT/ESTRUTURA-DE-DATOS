285a2caec9983661c668c8f60b296cc89ea26376
HANSQUISBERT <126077142+HANSQUISBERT@users.noreply.github.com>
2023-03-31 22:45:23 -0400

    Add files via upload

---
+++ Hito-2/Procesual/Codigo/src/Provincia.java
@@ -0,0 +1,17 @@
+public class Provincia {
+    private String Nombre;
+
+    public Provincia(){
+        Nombre = "";
+    }
+    public String getNombre(){
+        return Nombre;
+    }
+    public void setNombre(String nombre ){
+        this.Nombre = nombre;
+    }
+
+    public void mostrarProvincia(){
+        System.out.println("Provincia: "+this.getNombre());
+    }
+}
